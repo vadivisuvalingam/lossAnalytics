a1983e52974c90e5fcbc04557b98bd80655c2697
vadivisuvalingam <vadi.visuvalingan@gmail.com>
2017-02-27 00:35:50 -0500

    Repository Pattern
    Repository Pattern

---
+++ lossAnalytics/Base/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lossAnalytics.Base
+{
+    public class Entity
+    {
+        public string Id;
+    }
+}
---
+++ lossAnalytics/Base/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lossAnalytics.Base
+{
+    public interface IRepository<T> where T: Entity
+    {
+        IEnumerable<T> List { get; }
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+
+        T FindById(int Id);
+    }
+}
