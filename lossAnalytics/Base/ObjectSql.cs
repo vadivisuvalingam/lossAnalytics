2531907de89bba19db91ef2f5ece02f71b6d98e0
vadivisuvalingam <vadi.visuvalingan@gmail.com>
2017-03-20 00:33:19 -0400

    Adding QueryService.

---
+++ lossAnalytics/Base/ObjectSql.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lossAnalytics.Base
+{
+    public class ObjectSql
+    {
+        public ObjectSql()
+        {
+
+        }
+
+        public string Table()
+        {
+            return String.Empty;
+        }
+
+        public List<Column> Columns()
+        {
+            var retValue = new List<Column>();
+            return retValue;
+        }
+    }
+}
---
+++ lossAnalytics/Service/QueryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using lossAnalytics.Base;
+
+namespace lossAnalytics.Service
+{
+    public class QueryService
+    {
+        private ObjectSql _objectSql;
+
+        public QueryService(ObjectSql objectSql)
+        {
+            _objectSql = objectSql;
+        }
+    }
+}
