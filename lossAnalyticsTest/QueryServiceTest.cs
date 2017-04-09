83ee5760d2e159ebf863b7565922ab2397fd96d1
vadivisuvalingam <vadi.visuvalingan@gmail.com>
2017-04-09 12:36:02 -0400

    Adding test Initializer

--- lossAnalyticsTest/QueryServiceTest.cs
+++ lossAnalyticsTest/QueryServiceTest.cs
@@ -9,22 +9,34 @@ namespace lossAnalyticsTest
     [TestClass]
     public class QueryServiceTest
     {
+        Moq.Mock<IColumn> Column1, Column2, Column3, IdColumn;
+        Moq.Mock<IObjectSql> ObjectSql;
+        QueryService QueryService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Column1 = new Moq.Mock<IColumn>();
+            Column1.SetupProperty(x => x.ColumnName, "Column1");
+            Column2 = new Moq.Mock<IColumn>();
+            Column2.SetupProperty(x => x.ColumnName, "Column2");
+            Column3 = new Moq.Mock<IColumn>();
+            Column3.SetupProperty(x => x.ColumnName, "Column3");
+            var IdColumn = new Moq.Mock<IColumn>();
+            IdColumn.SetupProperty(x => x.ColumnName, "IdColumn");
+
+            ObjectSql = new Moq.Mock<IObjectSql>();
+            ObjectSql.SetupProperty(x => x.Table, "Table");
+            ObjectSql.SetupProperty(x => x.Id, IdColumn.Object);
+            ObjectSql.SetupProperty(x => x.Columns, new List<IColumn>() { Column1.Object, Column2.Object, Column3.Object });
+
+            QueryService = new QueryService(ObjectSql.Object);
+        }
+
         [TestMethod]
         public void TestSelect()
         {
             var ExpectedSelectStatement = "SELECT 'Column1','Column2','Column3' FROM 'Table'";
-            var Column1 = new Moq.Mock<IColumn>();
-            Column1.SetupProperty(x => x.ColumnName, "Column1");
-            var Column2 = new Moq.Mock<IColumn>();
-            Column2.SetupProperty(x => x.ColumnName, "Column2");
-            var Column3 = new Moq.Mock<IColumn>();
-            Column3.SetupProperty(x => x.ColumnName, "Column3");
-
-            var ObjectSql = new Moq.Mock<IObjectSql>();
-            ObjectSql.SetupProperty(x => x.Table, "Table");
-            ObjectSql.SetupProperty(x => x.Columns, new List<IColumn>() { Column1.Object, Column2.Object, Column3.Object });
-
-            var QueryService = new QueryService(ObjectSql.Object);
             var SelectString = QueryService.GetSelectStatement();
             Assert.AreEqual(ExpectedSelectStatement, SelectString);
         }
@@ -33,18 +45,6 @@ namespace lossAnalyticsTest
         public void TestInsert()
         {
             var ExpectedSelectStatement = "INSERT INTO 'Table'( 'Column1','Column2','Column3') VALUES ('value1','value2','value3')";
-            var Column1 = new Moq.Mock<IColumn>();
-            Column1.SetupProperty(x => x.ColumnName, "Column1");
-            var Column2 = new Moq.Mock<IColumn>();
-            Column2.SetupProperty(x => x.ColumnName, "Column2");
-            var Column3 = new Moq.Mock<IColumn>();
-            Column3.SetupProperty(x => x.ColumnName, "Column3");
-
-            var ObjectSql = new Moq.Mock<IObjectSql>();
-            ObjectSql.SetupProperty(x => x.Table, "Table");
-            ObjectSql.SetupProperty(x => x.Columns, new List<IColumn>() { Column1.Object, Column2.Object, Column3.Object });
-
-            var QueryService = new QueryService(ObjectSql.Object);
             var InsertString = QueryService.GetInsertStatement(new List<string>() { "value1", "value2", "value3" });
             Assert.AreEqual(ExpectedSelectStatement, InsertString);
         }
@@ -53,14 +53,6 @@ namespace lossAnalyticsTest
         public void TestDelete()
         {
             var ExpectedDeleteStatement = "DELETE * FROM 'Table'WHERE 'IdColumn' = '10'";
-            var IdColumn = new Moq.Mock<IColumn>();
-            IdColumn.SetupProperty(x => x.ColumnName, "IdColumn");
-
-            var ObjectSql = new Moq.Mock<IObjectSql>();
-            ObjectSql.SetupProperty(x => x.Table, "Table");
-            ObjectSql.SetupProperty(x => x.Id, IdColumn.Object);
-
-            var QueryService = new QueryService(ObjectSql.Object);
             var DeleteString = QueryService.GetDeleteStatement("10");
             Assert.AreEqual(ExpectedDeleteStatement, DeleteString);
         }
@@ -69,21 +61,6 @@ namespace lossAnalyticsTest
         public void TestUpdate()
         {
             var ExpectedUpdateStatement = "UPDATE 'Table' SET 'Column1' = 'value1','Column2' = 'value2','Column3' = 'value3' WHERE 'IdColumn' = '23' ";
-            var Column1 = new Moq.Mock<IColumn>();
-            Column1.SetupProperty(x => x.ColumnName, "Column1");
-            var Column2 = new Moq.Mock<IColumn>();
-            Column2.SetupProperty(x => x.ColumnName, "Column2");
-            var Column3 = new Moq.Mock<IColumn>();
-            Column3.SetupProperty(x => x.ColumnName, "Column3");
-
-            var IdColumn = new Moq.Mock<IColumn>();
-            IdColumn.SetupProperty(x => x.ColumnName, "IdColumn");
-
-            var ObjectSql = new Moq.Mock<IObjectSql>();
-            ObjectSql.SetupProperty(x => x.Table, "Table");
-            ObjectSql.SetupProperty(x => x.Id, IdColumn.Object);
-
-            var QueryService = new QueryService(ObjectSql.Object);
             var UpdateString = QueryService.GetUpdateStatement(new Dictionary<IColumn, String> () { { Column1.Object, "value1" }, { Column2.Object, "value2" }, { Column3.Object, "value3" } }, "23");
             Assert.AreEqual(ExpectedUpdateStatement, UpdateString);
         }
